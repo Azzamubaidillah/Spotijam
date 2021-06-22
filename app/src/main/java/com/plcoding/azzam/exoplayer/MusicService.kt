80c14cce6bcfac329a80f2deb3f34b3f5d3aee1e
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-06-22 22:42:28 +0700

    music service connection

--- app/src/main/java/com/plcoding/azzam/exoplayer/MusicService.kt
+++ app/src/main/java/com/plcoding/azzam/exoplayer/MusicService.kt
@@ -18,6 +18,7 @@ import com.plcoding.azzam.exoplayer.callbacks.MusicPlaybackPreparer
 import com.plcoding.azzam.exoplayer.callbacks.MusicPlayerEventListener
 import com.plcoding.azzam.exoplayer.callbacks.MusicPlayerNotificationListener
 import com.plcoding.azzam.other.Constants.MEDIA_ROOT_ID
+import com.plcoding.azzam.other.Constants.NETWORK_ERROR
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import javax.inject.Inject
@@ -156,6 +157,7 @@ class MusicService : MediaBrowserServiceCompat() {
                             isPlayerInitialized = true
                         }
                     } else {
+                        mediaSession.sendSessionEvent(NETWORK_ERROR, null)
                         result.sendResult(null)
                     }
                 }
--- app/src/main/java/com/plcoding/azzam/exoplayer/MusicServiceConnector.kt
+++ app/src/main/java/com/plcoding/azzam/exoplayer/MusicServiceConnector.kt
@@ -1,7 +1,10 @@
 package com.plcoding.azzam.exoplayer
 
+import android.content.ComponentName
 import android.content.Context
+import android.media.browse.MediaBrowser
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
@@ -28,8 +31,58 @@ class MusicServiceConnector(
 
     lateinit var mediaController: MediaControllerCompat
 
+    private val mediaBrowserConnectionCallback = MediaBrowserConnectionCallback(context)
+
+    private val mediaBrowser = MediaBrowserCompat(
+        context,
+        ComponentName(
+            context,
+            MusicService::class.java
+        ),
+        mediaBrowserConnectionCallback,
+        null
+    ).apply {
+        connect()
+    }
+
     val transportControls: MediaControllerCompat.TransportControls
         get() = mediaController.transportControls
+
+    fun subsribe(parentId: String, callback: MediaBrowserCompat.SubscriptionCallback){
+        mediaBrowser.subscribe(parentId, callback)
+    }
+
+    fun unsubsribe(parentId: String, callback: MediaBrowserCompat.SubscriptionCallback){
+        mediaBrowser.unsubscribe(parentId, callback)
+    }
+
+    private inner class MediaBrowserConnectionCallback(
+        private val context: Context
+    ) : MediaBrowserCompat.ConnectionCallback() {
+
+        override fun onConnected() {
+            mediaController = MediaControllerCompat(context, mediaBrowser.sessionToken).apply {
+                registerCallback(
+                    MediaControllerCallback()
+                )
+            }
+            _isConnected.postValue(Event(Resource.success(true)))
+        }
+
+        override fun onConnectionSuspended() {
+            _isConnected.postValue(Event(Resource.error(
+                "The connection was suspended", false
+            )))
+        }
+
+        override fun onConnectionFailed() {
+            _isConnected.postValue(Event(Resource.error(
+                "Couldnt connect to media browser", false
+            )
+            ))
+        }
+
+    }
 
     private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
         override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
@@ -53,5 +106,10 @@ class MusicServiceConnector(
                 )
             }
         }
+
+        override fun onSessionDestroyed() {
+            mediaBrowserConnectionCallback.onConnectionSuspended()
+        }
+
     }
 }

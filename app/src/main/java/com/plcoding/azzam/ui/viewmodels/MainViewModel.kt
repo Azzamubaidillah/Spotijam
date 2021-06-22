6f5120214cfe34ade45323977b100e91c7acbd33
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2021-06-22 23:22:10 +0700

    mainviewmodel

--- app/src/main/java/com/plcoding/azzam/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/plcoding/azzam/ui/viewmodels/MainViewModel.kt
@@ -1,12 +1,16 @@
 package com.plcoding.azzam.ui.viewmodels
 
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.plcoding.azzam.data.entities.Song
 import com.plcoding.azzam.exoplayer.MusicServiceConnector
+import com.plcoding.azzam.exoplayer.isPlayEnabled
+import com.plcoding.azzam.exoplayer.isPlaying
+import com.plcoding.azzam.exoplayer.isPrepared
 import com.plcoding.azzam.other.Constants.MEDIA_ROOT_ID
 import com.plcoding.azzam.other.Resource
 
@@ -54,6 +58,22 @@ class MainViewModel @ViewModelInject constructor(
 
     fun seekTo(pos: Long) {
         musicServiceConnection.transportControls.seekTo(pos)
+    }
+
+    fun playOrToggleSong(mediaItem: Song, toggle: Boolean = false) {
+        val isPrepared = playbackState.value?.isPrepared ?: false
+        if (isPrepared && mediaItem.mediaId ==
+            curPlaingSong?.value?.getString(METADATA_KEY_MEDIA_ID)) {
+            playbackState.value?.let { playbackState ->
+                when {
+                    playbackState.isPlaying -> if(toggle) musicServiceConnection.transportControls.pause()
+                    playbackState.isPlayEnabled -> musicServiceConnection.transportControls.play()
+                    else -> Unit
+                }
+            }
+        } else {
+            musicServiceConnection.transportControls.playFromMediaId(mediaItem.mediaId, null)
+        }
     }
 
     override fun onCleared() {
